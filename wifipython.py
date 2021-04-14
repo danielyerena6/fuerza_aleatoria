59124bd8af9aa92868052ac5b0a4318d4d5da8b6
blasterdaxx <blasterdaxx2208@gmail.com>
2021-04-14 10:59:04 -0500

    Version no funcional con wpa_supplicant

--- wifipython.py
+++ wifipython.py
@@ -1,11 +1,18 @@
 import random
+import os
+import subprocess
 
 caracteres = "abcdefghijklmnñopqrstuvwxyz0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
 
-passwd=""
+while True:
+    passwd=""
 
-for i in range(10):
-    passwd += caracteres[random.randrange(len(caracteres))]
+    for i in range(10):
+        passwd += caracteres[random.randrange(len(caracteres))]
 
-print(passwd)
+    print(passwd)
+
+
+    print(subprocess.call("wpa_passphrase TURED {} > red.conf".format(passwd),shell=True))
+    print(subprocess.call(" sudo wpa_supplicant -B -i wlan0  -c red.conf ".format(passwd),shell=True))
