f4e7253c858e5498622681ef4ed1f73bb5642845
blasterdaxx <blasterdaxx2208@gmail.com>
2021-04-16 23:40:50 -0500

    Version con seleccion de red, escaneo de redes y longitud de la clave

--- wifipython.py
+++ wifipython.py
@@ -1,18 +1,71 @@
+#!/usr/bin/env python3 
 import random
 import os
 import subprocess
+import sys
+from wifi import Cell
+import time
 
 caracteres = "abcdefghijklmnñopqrstuvwxyz0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+lista_redes=[]
+ssid=""
+contador=0
+n=0
+interfaz=""
+
+def escaneo_redes():
+    for cell in Cell.all(interfaz):
+        lista_redes.append(cell)
+    
 
 
-while True:
-    passwd=""
+def error():
+    print("Opcion no valida")
 
-    for i in range(10):
-        passwd += caracteres[random.randrange(len(caracteres))]
+def generador_claves():
+    print("Su informacion: \n ssid={} \n interfaz={}".format(ssid,interfaz))
+    time.sleep(1)
+    while True:
+        passwd=""
 
-    print(passwd)
+    
+
+        for i in range(n):
+            passwd += caracteres[random.randrange(len(caracteres))]
+
+        print(passwd)
 
 
-    print(subprocess.call("wpa_passphrase TURED {} > red.conf".format(passwd),shell=True))
-    print(subprocess.call(" sudo wpa_supplicant -B -i wlan0  -c red.conf ".format(passwd),shell=True))
+        print(subprocess.call("wpa_passphrase {} {} > red.conf".format(ssid,passwd),shell=True))
+        print(subprocess.call("sudo wpa_supplicant -B -i {}  -c red.conf ".format(interfaz),shell=True))
+
+interfaz=input("Ingrese su interfaz inalambrica: ")
+
+respuesta=input("Desea escanear las redes cercanas?? (S/n): ")
+respuesta.lower()
+print(respuesta)
+
+if(respuesta != "s" and respuesta != "n"):
+    error()
+elif respuesta=="s":
+    escaneo_redes()
+    print("Seleccione la red")
+
+    for red in lista_redes:
+        print("{}: {}".format(contador,red.ssid))
+        contador += 1
+
+    red_seleccionada=input("\n")
+    ssid=lista_redes[int(red_seleccionada)].ssid
+    interfaz=input("Ingrese su interfaz inalambrica: ")
+    n=int(input("Ingrese el numero de caracteres de su clave: "))
+    generador_claves()
+else:
+    ssid=input("Ingrese su ssid: ")
+    
+    n=int(input("Ingrese el numero de caracteres de su clave: "))
+    generador_claves()
+
+
+
+
